b87219a9dc5de29a5beacf7fc775ba0a5390d276
Eric Beauchner <39970368+ebeauchner@users.noreply.github.com>
2024-10-22 20:54:01 -0400

    added code to main.py to count each letter as lowercase and print report

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 with open("/Users/ericbeauchner/workspace/github.com/ebeauchner/bookbot/books/frankenstein.txt") as f:
     file_contents = f.read()
-print(file_contents)
+words_list = file_contents.split()
+words_list_length = len(words_list)
+file_contents_lower = file_contents.lower()
+letter_count = {}
+
+#lowercase ascii: 97-122
+for character in file_contents_lower:
+    if character in letter_count:
+        letter_count[character] += 1
+    elif ord(character) >= 97 and ord(character) <= 122:
+        letter_count[character] = 1
+ordered_count = dict(sorted(letter_count.items(), reverse=True, key=lambda item: item[1]))
+
+
+print("--- Begin report of books/frankenstein.txt ---")
+print(f"{words_list_length} words found in the document\n")
+for item in ordered_count:
+    print(f"The '{item}' character was found {letter_count[item]} times")
+print("--- End report ---")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+--- Begin report of books/frankenstein.txt ---
